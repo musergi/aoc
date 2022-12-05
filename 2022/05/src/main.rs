9b008ceff279bfde979e25f3369a5c066e4d86d5
musergi <sergi.mus@gmail.com>
2022-12-05 20:29:24 +0100

    Fixed merge conflict

--- 2022/05/src/main.rs
+++ 2022/05/src/main.rs
@@ -1,104 +1,33 @@
-use std::{
-    fs::File,
-    io::{self, BufRead},
-    num::ParseIntError,
-    str::FromStr,
-};
+use std::str::FromStr;
 
-struct Range {
-    start: u32,
-    end: u32,
+struct Stack {
+    crates: Vec<char>,
 }
 
-impl Range {
-    fn fully_contains(&self, other: &Self) -> bool {
-        self.start <= other.start && self.end >= other.end
-    }
-
-    fn intersect(&self, other: &Self) -> bool {
-        self.start <= other.end && other.start <= self.end
+impl Stack {
+    fn new() -> Stack {
+        Self { crates: Vec::new() }
     }
 }
 
-#[derive(Debug)]
-enum RangeParseError {
-    MissingStart(String),
-    MissingEnd(String),
-    BadNumberFormat(ParseIntError),
+struct Shipyard {
+    stacks: Vec<Stack>
 }
 
-impl From<ParseIntError> for RangeParseError {
-    fn from(err: ParseIntError) -> Self {
-        Self::BadNumberFormat(err)
-    }
-}
+struct ShipyardParseError;
 
-impl FromStr for Range {
-    type Err = RangeParseError;
+impl FromStr for Shipyard {
+    type Err = ShipyardParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut splits = s.split("-");
-        let start = splits
-            .next()
-            .ok_or(RangeParseError::MissingStart(s.to_string()))?
-            .parse::<u32>()?;
-        let end = splits
-            .next()
-            .ok_or(RangeParseError::MissingEnd(s.to_string()))?
-            .parse::<u32>()?;
-        Ok(Range { start, end })
-    }
-}
-
-struct ElfPair {
-    first: Range,
-    second: Range,
-}
-
-#[derive(Debug)]
-enum ElfPairParseError {
-    MissingFirst(String),
-    MissingSecond(String),
-    RangeParseError(RangeParseError),
-}
-
-impl From<RangeParseError> for ElfPairParseError {
-    fn from(err: RangeParseError) -> Self {
-        ElfPairParseError::RangeParseError(err)
-    }
-}
-
-impl FromStr for ElfPair {
-    type Err = ElfPairParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut splits = s.split(",");
-        let first = splits
-            .next()
-            .ok_or(ElfPairParseError::MissingFirst(s.to_string()))?
-            .parse::<Range>()?;
-        let second = splits
-            .next()
-            .ok_or(ElfPairParseError::MissingSecond(s.to_string()))?
-            .parse::<Range>()?;
-        Ok(ElfPair { first, second })
+        let lines = s.split("\n").collect::<Vec<_>>();
+        let stack_count = lines.get(lines.len() - 1).unwrap().trim().split_whitespace().count();
+        let mut stacks = Vec::with_capacity(stack_count);
+        
+        Ok(Shipyard { stacks })
     }
 }
 
 fn main() {
-    let f = File::open("assets/input.txt").expect("File");
-    let pairs = io::BufReader::new(f)
-        .lines()
-        .map(|l| l.expect("Line").parse::<ElfPair>().expect("Pair"))
-        .collect::<Vec<_>>();
-    let fully_contained = pairs
-        .iter()
-        .filter(|p| p.first.fully_contains(&p.second) || p.second.fully_contains(&p.first))
-        .count();
-    println!("Fully contained pairs: {}", fully_contained);
-    let intersecting = pairs
-        .iter()
-        .filter(|p| p.first.intersect(&p.second))
-        .count();
-    println!("Intersecting pairs: {}", intersecting);
+    println!("Hello, world!");
 }

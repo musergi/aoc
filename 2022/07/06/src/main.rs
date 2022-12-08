87aee3d692ff7c93ab691d9af4ab9e7b3d77b070
musergi <sergi.mus@gmail.com>
2022-12-08 17:36:27 +0100

    Finished day 7

--- 2022/07/06/src/main.rs
+++ 2022/07/06/src/main.rs
@@ -1,13 +1,17 @@
-use std::{num::ParseIntError, str::FromStr};
+use std::{fs, num::ParseIntError, str::FromStr};
 
 trait Sizeable {
     fn get_size(&self) -> usize;
 }
 
+#[derive(Debug)]
 struct Directory {
     name: String,
     content: Vec<DirectoryEntry>,
 }
+
+#[derive(Debug)]
+struct InvalidPath;
 
 impl Directory {
     fn new(s: String) -> Directory {
@@ -19,6 +23,47 @@ impl Directory {
 
     fn add(&mut self, entry: DirectoryEntry) {
         self.content.push(entry);
+    }
+
+    fn get_mut<I>(&mut self, mut path: I) -> Result<&mut Directory, InvalidPath>
+    where
+        I: Iterator<Item = String>,
+    {
+        match path.next() {
+            None => Ok(self),
+            Some(name) => self.dir_named_mut(&name).ok_or(InvalidPath)?.get_mut(path),
+        }
+    }
+
+    fn dir_named_mut(&mut self, name: &str) -> Option<&mut Directory> {
+        self.content
+            .iter_mut()
+            .filter_map(|e| match e {
+                DirectoryEntry::Directory(d) => Some(d),
+                DirectoryEntry::File(_) => None,
+            })
+            .find_map(|d| if d.name == name { Some(d) } else { None })
+    }
+}
+
+impl<'a> IntoIterator for &'a Directory {
+    type Item = &'a Directory;
+    type IntoIter = std::vec::IntoIter<&'a Directory>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        fn append<'a>(dir: &'a Directory, v: &mut Vec<&'a Directory>) {
+            v.push(dir);
+            for d in dir.content.iter().filter_map(|e| match e {
+                DirectoryEntry::Directory(d) => Some(d),
+                DirectoryEntry::File(_) => None,
+            }) {
+                append(d, v)
+            }
+        }
+
+        let mut result = vec![];
+        append(self, &mut result);
+        result.into_iter()
     }
 }
 
@@ -40,6 +85,7 @@ impl FromStr for Directory {
             let name = sp
                 .next()
                 .ok_or(DirectoryParseError::MissingSection)?
+                .trim()
                 .to_string();
             Ok(Directory::new(name))
         } else {
@@ -58,6 +104,7 @@ impl Sizeable for Directory {
     }
 }
 
+#[derive(Debug)]
 enum DirectoryEntry {
     Directory(Directory),
     File(File),
@@ -108,7 +155,7 @@ impl FromStr for File {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut sp = s.split_whitespace();
         let size = sp.next().ok_or(FileParseError)?.parse::<usize>()?;
-        let name = sp.next().ok_or(FileParseError)?.to_string();
+        let name = sp.next().ok_or(FileParseError)?.trim().to_string();
         Ok(File { name, size })
     }
 }
@@ -122,6 +169,7 @@ impl Sizeable for DirectoryEntry {
     }
 }
 
+#[derive(Debug)]
 struct File {
     name: String,
     size: usize,
@@ -133,19 +181,37 @@ impl Sizeable for File {
     }
 }
 
+#[derive(Debug)]
 enum Process {
     List(List),
-    Nav(),
+    Nav(Nav),
 }
 
+#[derive(Debug)]
 struct List {
     entries: Vec<DirectoryEntry>,
 }
 
+#[derive(Debug)]
 enum Nav {
     Root,
     Back,
     Dir(String),
+}
+
+#[derive(Debug)]
+struct NavParseError;
+
+impl FromStr for Nav {
+    type Err = NavParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "/" => Ok(Nav::Root),
+            ".." => Ok(Nav::Back),
+            _ => Ok(Nav::Dir(s.trim().to_string())),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -166,16 +232,25 @@ impl FromStr for Process {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut lines = s.split("\n");
-        let cmd = lines
+        let mut cmd = lines
             .next()
             .ok_or(ProcessParseError::Empty)?
             .split_whitespace();
         match cmd.next().ok_or(ProcessParseError::Empty)? {
             "ls" => {
                 let entries = lines
+                    .filter(|l| !l.is_empty())
                     .map(|l| l.parse::<DirectoryEntry>())
                     .collect::<Result<Vec<_>, _>>()?;
                 Ok(Process::List(List { entries }))
+            }
+            "cd" => {
+                let nav = cmd
+                    .next()
+                    .ok_or(ProcessParseError::Empty)?
+                    .parse::<Nav>()
+                    .unwrap();
+                Ok(Process::Nav(nav))
             }
             _ => Err(Self::Err::CommandNotFound),
         }
@@ -183,5 +258,44 @@ impl FromStr for Process {
 }
 
 fn main() {
-    let root = Directory::new("/".to_string());
+    let mut root = Directory::new("/".to_string());
+    let mut stack = Vec::new();
+    for process_res in fs::read_to_string("assets/input.txt")
+        .expect("File")
+        .split("$ ")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<Process>())
+    {
+        match process_res.expect("Command") {
+            Process::List(l) => {
+                for entry in l.entries {
+                    root.get_mut(stack.iter().cloned())
+                        .expect("Path must exist")
+                        .add(entry);
+                }
+            }
+            Process::Nav(n) => match n {
+                Nav::Root => stack.clear(),
+                Nav::Back => {
+                    stack.pop().expect("Cannot go back");
+                }
+                Nav::Dir(d) => stack.push(d),
+            },
+        }
+    }
+    let sum = root
+        .into_iter()
+        .map(|d| d.get_size())
+        .filter(|s| *s <= 100000)
+        .reduce(|a, b| a + b)
+        .expect("Total");
+    println!("Total small dirs: {}", sum);
+    let min_size = 30000000 - (70000000 - root.get_size());
+    let smallest = root
+        .into_iter()
+        .map(|d| d.get_size())
+        .filter(|s| *s > min_size)
+        .min()
+        .expect("Minimum");
+    println!("Minimum size removable: {}", smallest)
 }

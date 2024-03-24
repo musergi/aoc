47dfab4e2d1f9320b362ad343b076a0c61515dfb
smus <smus@zhilabs.com>
2024-03-24 18:15:43 +0100

    Add templates

---
+++ 2023/a-long-walk/src/main.rs
@@ -0,0 +1,14 @@
+use aoc::{aoc_main, input};
+use std::fmt::Display;
+
+fn main() {
+    aoc_main(input!(), part1, part2);
+}
+
+fn part1(_input: &str) -> impl Display {
+    0
+}
+
+fn part2(_input: &str) -> impl Display {
+    0
+}

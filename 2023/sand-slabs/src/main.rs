e5107eb63a006e2b9168a2a0e2e0198f77742521
smus <smus@zhilabs.com>
2024-03-30 12:15:18 +0100

    Fix formatting and add dependencies

--- 2023/sand-slabs/src/main.rs
+++ 2023/sand-slabs/src/main.rs
@@ -1,3 +1,15 @@
+use std::fmt::Display;
+
+use aoc::{aoc_main, input};
+
 fn main() {
-    println!("Hello, world!");
+    aoc_main(input!(), part1, part2);
+}
+
+fn part1(_input: &str) -> impl Display {
+    0
+}
+
+fn part2(_input: &str) -> impl Display {
+    0
 }
